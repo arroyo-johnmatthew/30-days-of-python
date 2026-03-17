2c2d9d33a589a0d447bf00e3c7feb47dc9056a84
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-17 23:53:38 +0800

    feat: implement day 1 exercise solution

---
+++ day_1/helloworld.py
@@ -0,0 +1,24 @@
+# Math Operations
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Strings
+print("John Matthew")
+print("Arroyo")
+print("Philippines")
+print("I am enjoying 30 days of python!")
+
+# Check Data Types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python',  'Finland']))
+print(type("John Matthew"))
+print(type("Arroyo"))
+print(type("Philippines"))
