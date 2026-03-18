a620102601e9800fecf2ec0a57d491043fe77de9
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-18 20:47:03 +0800

    feat(day-3): set up examples 1 and 2

---
+++ day_3/operators.py
@@ -0,0 +1,11 @@
+# Example 1
+# base = int(input("Enter a base: "))
+# height = float(input("Enter a height: "))
+# area = print("The area is: ", 0.5 * base * height)
+
+# Example 2
+side_a = int(input("Enter side a: "))
+side_b = int(input("Enter side b: "))
+side_c = int(input("Enter side c: "))
+perimeter = print("The perimiter of the triangle is: ", side_a + side_b + side_c)
+
