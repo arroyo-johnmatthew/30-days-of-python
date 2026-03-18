ef207b7a183f9a2986a7bc60721570fda361713a
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-18 20:51:43 +0800

    feat(day-3): provide example 3

--- day_3/operators.py
+++ day_3/operators.py
@@ -4,8 +4,15 @@
 # area = print("The area is: ", 0.5 * base * height)
 
 # Example 2
-side_a = int(input("Enter side a: "))
-side_b = int(input("Enter side b: "))
-side_c = int(input("Enter side c: "))
-perimeter = print("The perimiter of the triangle is: ", side_a + side_b + side_c)
+# side_a = int(input("Enter side a: "))
+# side_b = int(input("Enter side b: "))
+# side_c = int(input("Enter side c: "))
+# perimeter = print("The perimiter of the triangle is: ", side_a + side_b + side_c)
 
+# Example 3
+# length = int(input("Enter the length: "))
+# width = int(input("Enter the width: "))
+# rectangle_area = print("Area of the rectangle: ", length * width)
+# perimeter = print("The perimeter is: ", 2 * (length + width))
+
+# Example 4
