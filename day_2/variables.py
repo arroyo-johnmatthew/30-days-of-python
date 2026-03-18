df3f01dadf1a87f864ff58d7481fbeb6aad8ebf9
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-18 20:25:05 +0800

    feat: declare variables

---
+++ day_2/variables.py
@@ -0,0 +1,12 @@
+first_name = "John Matthew"
+last_name = "Arroyo"
+full_name = "John Matthew S. Arroyo"
+country = "Philippines"
+city = "Pasay City"
+age = 21
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+category, grade = "adventure", 10
+
