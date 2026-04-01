329cf753de8e8eec78aa9277b24489714dfe1aed
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-04-01 20:52:26 +0800

    feat: add day 6 exercise

---
+++ day_6/tuple.py
@@ -0,0 +1,11 @@
+empty_truffle = ()
+truffle_brothers = ("Traiven", "Justine")
+truffle_sisters = ("Tiffany", "Nicole", "Leah")
+
+siblings = truffle_brothers + truffle_sisters
+siblings_list = list(siblings)
+
+print("How many siblings do I have? ", len(siblings))
+
+
+
