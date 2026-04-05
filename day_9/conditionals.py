a530bf99c93c70e6177743582996ddcf417009d1
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-04-05 13:00:26 +0800

    feat(day_9): implement exercise level 2

--- day_9/conditionals.py
+++ day_9/conditionals.py
@@ -1,4 +1,5 @@
 # --------------------------- EXERCISE LEVEL 1 -----------------------------------
+
 # age = int(input("Enter your age: "))
 
 # # code if condition else code SHORTHAND Method
@@ -9,18 +10,69 @@
 # else:
 #     print("You need", 18 - age, "more years before you can drive!")
 
-my_age = 21
-your_age = int(input("Enter your age: "))
+# my_age = 21
+# your_age = int(input("Enter your age: "))
 
-if your_age > my_age:
-    if your_age - my_age == 1:
-        print("You are 1 year older than me!")
-    else:
-        print("You are", your_age - my_age, "years older than me")
-elif your_age < my_age:
-    if my_age - your_age == 1:
-        print("You are 1 year younger than me!")
-    else:
-        print("You are", my_age - your_age, "years younger than me")
-else:
-    print("We are the same age!")
+# if your_age > my_age:
+#     if your_age - my_age == 1:
+#         print("You are 1 year older than me!")
+#     else:
+#         print("You are", your_age - my_age, "years older than me")
+# elif your_age < my_age:
+#     if my_age - your_age == 1:
+#         print("You are 1 year younger than me!")
+#     else:
+#         print("You are", my_age - your_age, "years younger than me")
+# else:
+#     print("We are the same age!")
+
+# --------------------------- EXERCISE LEVEL 2 ---------------------------------
+
+# grade = int(input("Enter your grade: "))
+
+# if grade >= 90:
+#     print("Your grade is A")
+# elif grade <= 89 and grade >= 80:
+#     print("Your grade is B")
+# elif grade <= 79 and grade >= 70:
+#     print("Your grade is C")
+# elif grade <= 69 and grade >= 60:
+#     print("Your grade is D")
+# elif grade <= 59 and grade >= 50:
+#     print("Your grade is F")
+# else:
+#     print("You seem to have no grades 😓")
+
+# autumn = ("september", "october", "november")
+# winter = ("december", "january", "february")
+# spring = ("march", "april", "may")
+# summer = ("june", "july", "august")
+
+# month = input("Enter your month: ").lower()
+
+# for item in autumn:
+#     if month == item:
+#         print("Current Season: Autumn")
+    
+# for item in winter:
+#     if month == item:
+#         print("Current Season: Winter")
+
+# for item in spring:
+#     if month == item:
+#         print("Current Season: Spring")
+
+# for item in summer:
+#     if month == summer:
+#         print("Current Season: Summer")  
+# else: 
+#     print("Invalid") 
+
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# fruit = input("Add your fruit boi: ").lower()
+
+# if fruit in fruits:
+#     print("That fruit already exists!")
+# else:
+#     fruits.append(fruit)
+#     print("The modified fruit: ", fruits)
