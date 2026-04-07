086039cf0e335f144af15baab8c4be23583a69b6
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-04-07 21:41:22 +0800

    feat(day_10): implement exercise level 2

--- day_10/loops.py
+++ day_10/loops.py
@@ -1,52 +1,73 @@
 # --------------------------- EXERCISE LEVEL 1 ---------------------------------
-# Count 1 - 10
-count = 1
-while count < 11:
-    print(count)
-    count += 1
+# # Count 1 - 10
+# count = 1
+# while count < 11:
+#     print(count)
+#     count += 1
 
-# Count 10 - 1
-for item in range(10, 1, -1):
-    print(item)
+# # Count 10 - 1
+# for item in range(10, 1, -1):
+#     print(item)
 
-#
-##
-###
-####
-#####         Expected output
-######
-####### 
+# #
+# ##
+# ###
+# ####
+# #####         Expected output
+# ######
+# ####### 
 
-result = "#"
-while len(result) < 8:
-    print(result)
-    result += "#"
+# result = "#"
+# while len(result) < 8:
+#     print(result)
+#     result += "#"
 
-# # # # # # # # 
-# # # # # # # # 
-# # # # # # # # 
-# # # # # # # #  
-# # # # # # # #  Expected Output
-# # # # # # # # 
-# # # # # # # # 
-# # # # # # # # 
+# # # # # # # # # 
+# # # # # # # # # 
+# # # # # # # # # 
+# # # # # # # # #  
+# # # # # # # # #  Expected Output
+# # # # # # # # # 
+# # # # # # # # # 
+# # # # # # # # # 
 
-for col in range(8):
-    for row in range(8):
-        print("#", end=" ")
-    print("")
+# for col in range(8):
+#     for row in range(8):
+#         print("#", end=" ")
+#     print("")
 
-# Iterate through the list
-python_list = ["Python", "Flask", "Django", "FASTAPI", "Panda"]
-for item in python_list:
-    print(item, end=" ")
+# # Iterate through the list
+# python_list = ["Python", "Flask", "Django", "FASTAPI", "Panda"]
+# for item in python_list:
+#     print(item, end=" ")
 
-# 0 - 100 only print even numbers
-for num in range(0, 101):
+# # 0 - 100 only print even numbers
+# for num in range(0, 101):
+#     if num % 2 == 0:
+#         print(num)
+
+# # 0 - 100 only print odd numbers
+# for num in range(0, 101):
+#     if num % 2 != 0:
+#         print(num)
+
+# --------------------------- EXERCISE LEVEL 2 ---------------------------------
+# Print the sum of all numbers from 0 - 100
+number = 0
+for num in range(101):
+    number += num
+
+print("The total number:", number)
+
+even = 0
+odd = 0
+for num in range(101):
     if num % 2 == 0:
-        print(num)
+        even += num
+    elif num % 2 != 0:
+        odd += num
 
-# 0 - 100 only print odd numbers
-for num in range(0, 101):
-    if num % 2 != 0:
-        print(num)
+print("The total even:", even)
+print("The total odd:", odd)
+
+
