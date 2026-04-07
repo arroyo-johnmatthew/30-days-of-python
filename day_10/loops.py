4fe9f670a89854446f50797ff375e5830bb0a340
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-04-07 18:41:51 +0800

    feat(day_10): finish exercise level 1

--- day_10/loops.py
+++ day_10/loops.py
@@ -1,3 +1,4 @@
+# --------------------------- EXERCISE LEVEL 1 ---------------------------------
 # Count 1 - 10
 count = 1
 while count < 11:
@@ -15,8 +16,37 @@ for item in range(10, 1, -1):
 #####         Expected output
 ######
 ####### 
+
 result = "#"
 while len(result) < 8:
     print(result)
     result += "#"
 
+# # # # # # # # 
+# # # # # # # # 
+# # # # # # # # 
+# # # # # # # #  
+# # # # # # # #  Expected Output
+# # # # # # # # 
+# # # # # # # # 
+# # # # # # # # 
+
+for col in range(8):
+    for row in range(8):
+        print("#", end=" ")
+    print("")
+
+# Iterate through the list
+python_list = ["Python", "Flask", "Django", "FASTAPI", "Panda"]
+for item in python_list:
+    print(item, end=" ")
+
+# 0 - 100 only print even numbers
+for num in range(0, 101):
+    if num % 2 == 0:
+        print(num)
+
+# 0 - 100 only print odd numbers
+for num in range(0, 101):
+    if num % 2 != 0:
+        print(num)
