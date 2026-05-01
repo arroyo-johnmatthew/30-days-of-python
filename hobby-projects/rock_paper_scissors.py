3b94b62b6e196704055115e148797ca38470a36c
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-05-01 22:40:02 +0800

    feat: add line spaces and exception handling

--- hobby-projects/rock_paper_scissors.py
+++ hobby-projects/rock_paper_scissors.py
@@ -3,20 +3,27 @@ import random
 computer = ("rock", "paper", "scissors")
 comp = random.choice(computer)
 
-while True:
-    user = input("Enter your choice (rock 🪨 paper 📃 scissors ✂️): ").lower()
+try: 
+    while True:
+        user = input("Enter your choice: rock | paper | scissors ").lower()
 
-    if user not in computer:
-        print("Not a valid option. Try again")
-        continue
-
-    if user == "rock" and comp == "scissors":
-        print("You won! computer chose", comp)
-    elif user == "paper" and comp == "rock":
-        print("You won! computer chose", comp)
-    elif user == "scissors" and comp == "paper":
-        print("You won! computer chose", comp)
-    elif user == comp:
-        print("Tie!")
-    else:
-        print("You lose! computer chose", comp)
+        if user not in computer:
+            print("Not a valid option. Try again")
+            continue
+        
+        if user == "rock" and comp == "scissors":
+            print("You won! computer chose", comp)
+            print(" ")
+        elif user == "paper" and comp == "rock":
+            print("You won! computer chose", comp)
+            print(" ")
+        elif user == "scissors" and comp == "paper":
+            print("You won! computer chose", comp)
+            print(" ")
+        elif user == comp:
+            print("Tie!")
+            print(" ")
+        else:
+            print("You lose! computer chose", comp)
+except EOFError:
+    print()
