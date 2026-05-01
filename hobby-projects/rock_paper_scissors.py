25f72f25c9a23702abfa70f4fe070a37d61f77f7
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-05-01 22:47:37 +0800

    style: add emojis for visual presentation of the program

--- hobby-projects/rock_paper_scissors.py
+++ hobby-projects/rock_paper_scissors.py
@@ -5,25 +5,28 @@ comp = random.choice(computer)
 
 try: 
     while True:
+        print("⚠️  Press ctrl + z + ENTER to exit")
         user = input("Enter your choice: rock | paper | scissors ").lower()
+        print("")
 
         if user not in computer:
-            print("Not a valid option. Try again")
+            print("⚠️  Not a valid option. Try again")
             continue
         
         if user == "rock" and comp == "scissors":
-            print("You won! computer chose", comp)
+            print("🥳🙌  You won! computer chose", comp)
             print(" ")
         elif user == "paper" and comp == "rock":
-            print("You won! computer chose", comp)
+            print("🥳🙌  You won! computer chose", comp)
             print(" ")
         elif user == "scissors" and comp == "paper":
-            print("You won! computer chose", comp)
+            print("🥳🙌  You won! computer chose", comp)
             print(" ")
         elif user == comp:
-            print("Tie!")
+            print("😱  Tie!")
             print(" ")
         else:
-            print("You lose! computer chose", comp)
+            print("😭😵  You lose! computer chose", comp)
+            print(" ")
 except EOFError:
     print()
