ef7a481ae68c88a35b44b7b92b367c02035a05ce
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-05-02 12:38:59 +0800

    feat: start a new project (slot machine)

---
+++ hobby-projects/slot_machine.py
@@ -0,0 +1,16 @@
+import random
+
+def spin_slot():
+    pass
+
+def print_slot():
+    pass
+
+def main():
+    while True:
+        print("Welcome to Misty's Gamble")
+        print("Choose from 🍒 🍌 🍎")
+        bet = int(input("Place your bet: "))
+        print("")
+
+main()
