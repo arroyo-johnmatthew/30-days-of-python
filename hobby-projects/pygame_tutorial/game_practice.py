aa8542a56d765fe6175eeb35f8956fee8eb1295d
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-05-24 20:47:49 +0800

    feat: define advanced game information and dependencies

--- hobby-projects/pygame_tutorial/game_practice.py
+++ hobby-projects/pygame_tutorial/game_practice.py
@@ -5,6 +5,22 @@ import sys
 # initialize pygame
 pygame.init()
 
-# start the game loop
-while True:
-    pygame.display.update()
+# limiting frames
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+# predefined colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# screen information
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+# set up display
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
