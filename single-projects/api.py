aa8a49af724ae34f656f3e1e834879eb10b55306
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-06-14 22:45:02 +0800

    feat(api): change the output from track name to artwork url for experimentation

--- single-projects/api.py
+++ single-projects/api.py
@@ -5,8 +5,8 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1000&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=5&term=" + sys.argv[1])
 name = response.json()
 
 for result in name["results"]:
-    print(result["trackName"])
+    print(result["artworkUrl100"])
