0237961cefd0adbae7c22460b3a64e97a34120b1
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-06-15 20:32:22 +0800

    refactor(api): change the codebase into a practice playground to test CS50 api exercise

--- single-projects/api.py
+++ single-projects/api.py
@@ -1,11 +1,12 @@
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit()
+# checks if the cli argument is enough 
+if len(sys.argv) < 2:
+    sys.exit("Missing command-line argument")
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=5&term=" + sys.argv[1])
-name = response.json()
+# http get request from the server
+response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=e14647999e0018b436afce88cfeca71830417bc42650e63825a60bc337cc4394")
+# put the response in the price variable and convert it into a float
+price = float(response.json()["data"]["priceUsd"])
 
-for result in name["results"]:
-    print(result["artworkUrl100"])
