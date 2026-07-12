5427c8055c696f988622dece19c51257e8ac6f00
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-12 19:28:25 +0800

    feat(format): implement a simple name reformat using regex

---
+++ simple/CS50/WEEK7_REGEX/format.py
@@ -0,0 +1,9 @@
+import  re
+
+name = input("Enter your name: ").strip()
+matches = re.search(r"^(.+),\s?(.+)$", name)
+
+if matches:
+    name = matches.group(2) + " " + matches.group(1)
+
+print(name)
