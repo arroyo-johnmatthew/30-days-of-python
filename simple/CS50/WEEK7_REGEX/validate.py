32cbbbb0cf8604750fceeaedd1cc8ed9fc3781e6
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-12 18:15:44 +0800

    feat(validate): add more comments for clarity

--- simple/CS50/WEEK7_REGEX/validate.py
+++ simple/CS50/WEEK7_REGEX/validate.py
@@ -3,11 +3,15 @@ import re
 email = input("Email: ").strip()
 
 # '.' means any char
-# '*' means 0 or more chars
-# '+' means 1 or more chars
-# '\' means esc char, use to differentiate '.' and literal '.'
+# '*' 0 or more chars
+# '+' 1 or more chars
+# '\' esc char, use to differentiate '.' and literal '.'
+# '^' must match the start of the str
+# '$' must match the end of the str
+# '[]' set of chars
+# '[^]' complementing the set, for ex, '[^@]' means any string but not the '@'
 
-if re.search(r"^.+@.+\.edu$", email):
+if re.search(r"^[^@]+@[^@]+\.edu$", email):
     print("Valid")
 else:
     print("Invalid")
