bda8612c42a65841cb3649517943e5ea7d760c29
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-14 01:15:54 +0800

    feat(response.py): add simple email validator using the validators library

---
+++ simple/CS50/WEEK7_REGEX/response.py
@@ -0,0 +1,12 @@
+import validators
+
+def main():
+    print(validate(input("What's your email address? ")))
+
+def validate(email):
+    match = validators.email(email)
+
+    if match:
+        return "Valid"
+    else:
+        return "Invalid"
