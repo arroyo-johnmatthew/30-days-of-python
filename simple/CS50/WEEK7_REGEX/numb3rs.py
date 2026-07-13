638d006edbb64da1d830b8bd21f97a5b4a66061c
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-13 12:33:20 +0800

    feat(numb3rs): implement the final functionality of the code with its final unit test file

--- simple/CS50/WEEK7_REGEX/numb3rs.py
+++ simple/CS50/WEEK7_REGEX/numb3rs.py
@@ -3,13 +3,15 @@ import re
 def main():
     print(validate(input("IPv4 Address: ")))
 
-def validate(address):
-    matches = re.search(r"", address, re.IGNORECASE)
+def validate(address):    
+    # Assign the pattern into a var with these ranges: 0-99|100-199|200-249|250-255
+    ptrn = r"([1-9]?[0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])"
 
+    matches = re.search(fr"^{ptrn}\.{ptrn}\.{ptrn}\.{ptrn}$", address, re.IGNORECASE)
     if matches:
         return True
     else:
         return False
 
 if __name__ == "__main__":
-    main()
+    main() 
--- simple/CS50/WEEK7_REGEX/test_numb3rs.py
+++ simple/CS50/WEEK7_REGEX/test_numb3rs.py
@@ -1,1 +1,42 @@
 from numb3rs import validate
+
+# These are the unit tests I did to test the number ranges (0-99, 100-199, 200-249, and 250-255)
+# def test_0_to_99():
+#     assert validate("0") == True
+#     assert validate("98") == True
+#     assert validate("99") == True
+
+# def test_100_to_199():
+#     assert validate("100") == True
+#     assert validate("101") == True
+#     assert validate("199") == True
+
+# def test_200_to_249():
+#     assert validate("200") == True
+#     assert validate("201") == True
+#     assert validate("248") == True
+#     assert validate("249") == True
+
+# def test_250_to_255():
+#     assert validate("250") == True
+#     assert validate("254") == True
+#     assert validate("255") == True
+#     assert validate("256") == False
+
+# def zero_trailings():
+#     assert validate("000") == False
+#     assert validate("001") == False
+#     assert validate("012") == False
+#     assert validate("099") == False
+#     assert validate("00") == False
+#     assert validate("09") == False
+#     assert validate("009") == False
+
+def test_ip():
+    assert validate("1.2.3.4") == True
+    assert validate("127.0.0.1") == True
+    assert validate("255.255.255.255") == True
+    assert validate("512.512.512.512") == False
+    assert validate("1.2.3.1000") == False
+    assert validate("192.168.001.1") == False
+    assert validate("cat") == False
