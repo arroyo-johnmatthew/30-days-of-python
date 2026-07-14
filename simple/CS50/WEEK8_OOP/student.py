f9609f7832f5dd40770f60abd6c09b64c5b6ae90
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-14 12:17:46 +0800

    feat(student): apply a Student class and object instantiation

--- simple/CS50/WEEK8_OOP/student.py
+++ simple/CS50/WEEK8_OOP/student.py
@@ -1,16 +1,17 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
 
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student["name"]} from {student["house"]}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     name = input("Name: ")
-    house = input("House")
-    return {"name": name, "house": house}
+    house = input("House: ")
+    return Student(name, house)
+
 
 if __name__ == "__main__":
     main()
