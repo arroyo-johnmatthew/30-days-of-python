22f3948fb53cbe8e41d37324837650241fefa660
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-16 22:20:19 +0800

    feat(jar): implement the complete Jar class with all its methods

--- simple/CS50/WEEK8_OOP/jar.py
+++ simple/CS50/WEEK8_OOP/jar.py
@@ -1,19 +1,22 @@
 class Jar:
-    def __init__(self, capacity=12):
+    def __init__(self, capacity=12, size=0):
         self.capacity = capacity
+        self.size = size
 
     def __str__(self):
         return "🍪" * self.size
 
-
     def deposit(self, n):
-        if n > self.capacity:
+        if n + self.size > self.capacity:
             raise ValueError("Cant fit it all cuz")
         else:
             self.size += n
 
     def withdraw(self, n):  
-        ...
+        if n > self.size:
+            raise ValueError("Not enough cookies to take lil bro")
+        else:
+            self.size -= n
 
     # Capacity GETTER and SETTER
     @property
@@ -34,7 +37,10 @@ class Jar:
         self._size = size
 
 def main():
-    ...
+    jar1 = Jar()
+    jar1.deposit(10)
+    jar1.withdraw(5)
+    print(jar1)
 
 if __name__ == "__main__":
     main()
