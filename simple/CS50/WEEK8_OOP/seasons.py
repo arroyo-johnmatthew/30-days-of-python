e404b65beff8acce90baff96f328433ed6f05a8c
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-16 17:59:12 +0800

    feat: improve test code and logic code

--- simple/CS50/WEEK8_OOP/seasons.py
+++ simple/CS50/WEEK8_OOP/seasons.py
@@ -13,7 +13,7 @@ def main():
     year, month, day = split_date(birthdate, format)
     birthdate = date(year, month, day)
 
-    # Create date object then subtract it with the current date
+    # Get the minutes then convert it into words
     age_in_mins = get_age_in_min(date.today(), birthdate)
     print(num_to_words(age_in_mins))
     
@@ -23,7 +23,7 @@ def get_age_in_min(current, bday):
 
 def num_to_words(num):
     p = inflect.engine()
-    return f"{str(p.number_to_words(num)).capitalize()} minutes"
+    return f"{str(p.number_to_words(num, andword="")).capitalize()} minutes"
 
 def get_format():
     return r"^([1-9][0-9]{3})-((?:0[1-9]|1[0-2]))-((?:0[1-9]|[12][0-9]|3[01]))$"
---
+++ simple/CS50/WEEK8_OOP/test_seasons.py
@@ -0,0 +1,19 @@
+import pytest
+from seasons import num_to_words, validate_bday
+
+def test_mins():
+    assert num_to_words(525600) == "Five hundred twenty-five thousand, six hundred minutes"
+    assert num_to_words(1440) == "One thousand, four hundred forty minutes"
+    assert num_to_words(11327040) == "Eleven million, three hundred twenty-seven thousand forty minutes"
+    assert num_to_words(1051200) == "One million, fifty-one thousand, two hundred minutes"
+
+def test_validation():
+    format = r"^([1-9][0-9]{3})-((?:0[1-9]|1[0-2]))-((?:0[1-9]|[12][0-9]|3[01]))$"
+
+    with pytest.raises(SystemExit):
+        validate_bday("2026/07/16", format)
+    with pytest.raises(SystemExit):
+        validate_bday("07-16-2026", format)
+    with pytest.raises(SystemExit):
+        validate_bday("July 16, 2026", format)
+
