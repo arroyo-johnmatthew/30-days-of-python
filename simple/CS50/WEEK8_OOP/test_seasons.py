f060d33071bbf1893ce91eadaaeb5900d86ab248
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-16 18:01:26 +0800

    test: change assertions in test_mins func

--- simple/CS50/WEEK8_OOP/test_seasons.py
+++ simple/CS50/WEEK8_OOP/test_seasons.py
@@ -3,9 +3,9 @@ from seasons import num_to_words, validate_bday
 
 def test_mins():
     assert num_to_words(525600) == "Five hundred twenty-five thousand, six hundred minutes"
-    assert num_to_words(1440) == "One thousand, four hundred forty minutes"
-    assert num_to_words(11327040) == "Eleven million, three hundred twenty-seven thousand forty minutes"
+    assert num_to_words(527040) == "Five hundred twenty-seven thousand forty minutes"
     assert num_to_words(1051200) == "One million, fifty-one thousand, two hundred minutes"
+    assert num_to_words(11327040) == "Eleven million, three hundred twenty-seven thousand forty minutes"
 
 def test_validation():
     format = r"^([1-9][0-9]{3})-((?:0[1-9]|1[0-2]))-((?:0[1-9]|[12][0-9]|3[01]))$"
