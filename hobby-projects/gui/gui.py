928531a6f3571da7a8b964ac5aac558be7308432
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-17 23:40:08 +0800

    feat(gui): apply initial GUI

--- hobby-projects/gui/gui.py
+++ hobby-projects/gui/gui.py
@@ -1,15 +1,51 @@
 import tkinter as tk
+from tkinter import ttk
 
-# Set the root and its width and height
-root = tk.Tk()
-root.geometry("500x500")
+def validate():
+    pass
 
-# Set the root's title and icon
-root.title("CS50 Final Project")
-icon = tk.PhotoImage(file="assets/luffy.png")
-root.iconphoto(True, icon)
+def switch_frame(show, hide):
+    hide.pack_forget()
+    show.pack(fill="both", expand=True)
 
-root.config(background="gray")
+def main():
+    # set root widget
+    root = tk.Tk()
+    root.geometry("500x350")
+    root.title("Corporate Buddy")
+    root.configure(background="#FFFFFF")
+    root.resizable(False, False)
 
-# Run the root gui
-root.mainloop()
+    # ++ MAIN FRAME ++
+    main_frame = tk.Frame(root)
+    main_frame.configure(bg="white")
+
+    # set the text
+    label = tk.Label(main_frame, text="🇵🇭 Corporate Buddy", font=("Arial", 21, "bold"), bg="white")
+    label.pack(pady=20)
+
+    # set the buttons
+    for i in ["Validate ID", "Register IDs", "Others"]:
+        button = tk.Button(main_frame,
+                        text=i, 
+                        font=("Arial", 11, "bold"), 
+                        width=10,
+                        relief="raised",
+                        bg="#a9d6e5",
+                        activebackground="white",
+                        activeforeground="#a9d6e5",
+                        fg="white",
+                        command=lambda: switch_frame(val_frame, main_frame),
+                        cursor="hand2",
+                        bd=0,
+                        padx=20)
+        button.pack(pady=8)
+
+    # ++ Validation FRAME ++ 
+    val_frame = tk.Frame(root)
+
+    main_frame.pack(fill="both", expand=True)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
