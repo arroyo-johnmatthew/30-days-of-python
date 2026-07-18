7c04909d41cbfe43f5530c4e27e4830daabf2000
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-07-18 15:25:48 +0800

    feat(gui): implement initial structure with frame switching

--- hobby-projects/gui/gui.py
+++ hobby-projects/gui/gui.py
@@ -16,16 +16,19 @@ def main():
     root.configure(background="#FFFFFF")
     root.resizable(False, False)
 
-    # ++ MAIN FRAME ++
+    # =====================    MAIN FRAME  ============================= 
     main_frame = tk.Frame(root)
     main_frame.configure(bg="white")
 
     # set the text
-    label = tk.Label(main_frame, text="🇵🇭 Corporate Buddy", font=("Arial", 21, "bold"), bg="white")
+    label = tk.Label(main_frame, 
+                     text="🇵🇭 Corporate Buddy", 
+                     font=("Arial", 21, "bold"), 
+                     bg="white")
     label.pack(pady=20)
 
     # set the buttons
-    for i in ["Validate ID", "Register IDs", "Others"]:
+    for i in ["Tax Calculator", "Setup Your Government Contributions", "Others"]:
         button = tk.Button(main_frame,
                         text=i, 
                         font=("Arial", 11, "bold"), 
@@ -41,9 +44,27 @@ def main():
                         padx=20)
         button.pack(pady=8)
 
-    # ++ Validation FRAME ++ 
+    # =====================    VALIDATION FRAME  =============================  
     val_frame = tk.Frame(root)
+    val_frame.config(bg="white")
+    label = tk.Label(val_frame, text="Hello World", bg="white")
+    label.pack()
+    button = tk.Button(val_frame,
+                        text="⬅️ Return to Menu", 
+                        font=("Arial", 11, "bold"), 
+                        width=13,
+                        relief="raised",
+                        bg="#a9d6e5",
+                        activebackground="white",
+                        activeforeground="#a9d6e5",
+                        fg="white",
+                        command=lambda: switch_frame(main_frame, val_frame),
+                        cursor="hand2",
+                        bd=0,
+                        padx=30)
+    button.pack(pady=20, side="bottom")
 
+    # run the GUI
     main_frame.pack(fill="both", expand=True)
     root.mainloop()
 
